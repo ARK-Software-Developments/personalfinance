17ab79c72058777b039f7c41e9e4b1b30f98a526
Andres Kamycki <andres.kamycki@gmail.com>
2025-12-02 21:50:48 -0300

    delete all

--- src/front/interfaces/MenuItem.tsx
+++
@@ -1,7 +0,0 @@
-// Interfaz para los elementos del menú
-export interface MenuItem {
-    label: string;
-    href?: string; // Enlace opcional para items sin submenú
-    subItems?: MenuItem[]; // Submenús opcionales
-    title: string;
-}
