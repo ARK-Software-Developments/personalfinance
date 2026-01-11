7c57886c7dc03546055501675cd7c4b2af88c718
Andres Kamycki <andres.kamycki@gmail.com>
2026-01-11 00:23:27 -0300

    Sistema de alertar al Ingresar al sistema

---
+++ Models/Notificaciones/Notificacion.cs
@@ -0,0 +1,59 @@
+﻿namespace PersonalFinance.Models.Notificaciones
+{
+    using Newtonsoft.Json;
+    using PersonalFinance.Models;
+    using System.Text.Json.Serialization;
+
+    /// <summary>
+    /// Clase Notificacion.
+    /// </summary>
+    public class Notificacion : AbstractModelExternder
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Notificacion"/> class.
+        /// </summary>
+        public Notificacion()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets propiedad notificationdate.
+        /// </summary>
+        public DateTime FechaNotificacion { get; set; }
+
+        /// <summary>
+        /// Gets or sets propiedad title.
+        /// </summary>
+        public string Titulo { get; set; }
+
+        /// <summary>
+        /// Gets or sets propiedad type.
+        /// </summary>
+        public string Tipo { get; set; }
+
+        /// <summary>
+        /// Gets or sets propiedad messaje.
+        /// </summary>
+        public string Mensaje { get; set; }
+
+        /// <summary>
+        /// Gets or sets propiedad to.
+        /// </summary>
+        public string Para { get; set; }
+
+        /// <summary>
+        /// Gets or sets propiedad app.
+        /// </summary>
+        public string Aplicacion { get; set; }
+
+        /// <summary>
+        /// Gets or sets propiedad level.
+        /// </summary>
+        public string Nivel { get; set; }
+
+        /// <summary>
+        /// Gets or sets propiedad img.
+        /// </summary>
+        public string Imagen { get; set; }
+    }
+}
