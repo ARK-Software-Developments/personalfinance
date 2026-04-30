45daa332dcd5ecdfb80882875974724a05ec08f1
Andres Kamycki <andres.kamycki@gmail.com>
2026-04-30 19:30:30 -0300

    align prestamos

---
+++ Models/Tarjetas/ResumenTC.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace PersonalFinance.Models.Tarjetas
+{
+    public class ResumenTC
+    {
+        public ResumenTC() { }
+
+        public int TotalAcumulado { get; set; }
+
+        public string NombreRef { get; set; }
+    }
+}
